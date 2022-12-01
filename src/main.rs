01aea88df7528fb24e7e3976e737e498bd1f19b2
Arthur Damasceno <damascenoarthur653@gmail.com>
2022-12-01 16:44:27 +0000

    feat(args): program arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use {clap::Parser, std::path::PathBuf};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// Database file name to read.
+    #[arg(short, long)]
+    read: Option<PathBuf>,
+    /// File name to create as a database.
+    #[arg(short, long)]
+    create: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{args:#?}");
 }
